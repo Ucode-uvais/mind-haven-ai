a5d1083b467d14a404ba5fa7e56e77e5d4201290
Seyyed Uvais <seyyeduvais@gmail.com>
2025-07-23 07:29:39 +0530

    footer implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Providers } from "@/components/providers";
+import { Footer } from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           <Header />
 
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="border-t py-6">
+      <div className="container flex flex-col items-center gap-4 px-4 md:px-6">
+        <p className="text-sm text-muted-foreground text-center">
+          © 2025 Mind Haven 2.0 All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
